bf896833e38d701a8d4833327f5c8d9747ef2966
Cora Iberkleid <ciberkleid@gmail.com>
2020-08-14 14:52:02 -0400

    fix syntax of cookie in test

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 echo "basic"
-for i in {1..6}; do curl --location --request GET "http://blueorgreen.marygabry.name/color" --header "type=none"; echo; done
+for i in {1..6}; do curl --location --request GET "http://blueorgreen.marygabry.name/blueorgreen" --header 'cookie: type=none'; echo; done
 
 echo "premium"
-for i in {1..6}; do curl --location --request GET "http://blueorgreen.marygabry.name/color" --header "type=premium"; echo; done
+for i in {1..6}; do curl --location --request GET "http://blueorgreen.marygabry.name/blueorgreen" --header 'cookie: type=premium'; echo; done
