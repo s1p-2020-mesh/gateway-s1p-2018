e19812d99e894438e572851379d0d89d71e323db
Cora Iberkleid <ciberkleid@gmail.com>
2020-08-15 22:29:40 -0400

    new file to switch accounts

---
+++ switch-accounts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [[ $1 == "cora" ]]; then
+
+  sed -i '' 's/marygabry1508/ciberkleid/g' docker-push.sh
+  sed -i '' 's/marygabry\.name/coraiberkleid\.xyz/g' test.sh
+  sed -i '' 's/marygabry\.name/coraiberkleid\.xyz/g' istio-demo-steps.sh
+
+  pushd yaml/eureka
+  sed -i '' 's/marygabry1508/ciberkleid/g' *.yaml
+  popd
+
+  pushd yaml/istio
+  sed -i '' 's/marygabry1508/ciberkleid/g' *.yaml
+  sed -i '' 's/marygabry\.name/coraiberkleid\.xyz/g' *.yaml
+  popd
+
+else
+
+  sed -i '' 's/ciberkleid/marygabry1508/g' docker-push.sh
+  sed -i '' 's/coraiberkleid\.xyz/marygabry\.name/g' test.sh
+  sed -i '' 's/coraiberkleid\.xyz/marygabry\.name/g' istio-demo-steps.sh
+
+  pushd yaml/eureka
+  sed -i '' 's/ciberkleid/marygabry1508/g' *.yaml
+  popd
+
+  pushd yaml/istio
+  sed -i '' 's/ciberkleid/marygabry1508/g' *.yaml
+  sed -i '' 's/coraiberkleid\.xyz/marygabry\.name/g' *.yaml
+  popd
+
+fi
+
+
