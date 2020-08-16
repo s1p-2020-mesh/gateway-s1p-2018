cea9428c02beaa2ee1b58c68b848ade3255e74f1
Cora Iberkleid <ciberkleid@gmail.com>
2020-08-15 23:59:46 -0400

    added logging

--- blueorgreenservice/src/main/java/org/springframework/demo/ColorController.java
+++ blueorgreenservice/src/main/java/org/springframework/demo/ColorController.java
@@ -1,5 +1,7 @@
 package org.springframework.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ColorController {
 
+	private static final Logger log = LoggerFactory.getLogger(ColorController.class);
+
 	private ColorProperties colorProperties;
 
 	public  ColorController(ColorProperties colorProperties) {
@@ -17,9 +21,12 @@ public class ColorController {
 
 	@RequestMapping
 	public Color color() throws InterruptedException {
+		log.info("Got request for " + colorProperties.getColor());
 		if(colorProperties.isSlow()) {
+			log.info("Sleeping...");
 			Thread.sleep(5000);
 		}
+		log.info("Sending response for " + colorProperties.getColor());
 		if(Color.BLUE.getId().equalsIgnoreCase(colorProperties.getColor())) {
 			return Color.BLUE;
 		} else if(Color.YELLOW.getId().equalsIgnoreCase(colorProperties.getColor())) {
