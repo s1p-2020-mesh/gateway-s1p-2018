60414f6469b15b598919ce778538c697dd558bc0
Cora Iberkleid <ciberkleid@gmail.com>
2020-08-23 14:17:18 -0400

    cleanup

--- istio-run.sh
+++ istio-run.sh
@@ -7,3 +7,6 @@ kubectl label namespace $NS istio-injection=enabled
 kubectl config set-context $(kubectl config current-context) --namespace=$NS
 
 kubectl apply -f yaml/istio/
+
+
+# for i in {1..12}; do time curl --location --request GET "http://blueorgreen.springone.coraiberkleid.xyz/blueorgreen" --header "cookie: type=premium"; echo; done
